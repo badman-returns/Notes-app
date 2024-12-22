1535671cca6746282fe5001c28f6452fe8e942e3
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2024-12-22 17:09:07 +0530

    feat: :sparless: Add note functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import NoteController from './components/NoteController/NoteController';
 import NoteTable from './components/NotesTable/NoteTable';
 
 function App() {
-
+  const [notes, setNotes] = useState([]);
+  console.log(notes);
   return (
     <div className='note-container'>
       <h1>Notes app</h1>
       <div className="note-controller"> 
-        <NoteController />
+        <NoteController notes={notes} setNote={setNotes}/>
       </div>
       <div className="notes-table">
-        <NoteTable />
+        <NoteTable notes={notes}/>
       </div>
     </div>
   )
--- src/components/NoteController/NoteController.jsx
+++ src/components/NoteController/NoteController.jsx
@@ -1,13 +1,38 @@
 
+import { useState } from 'react';
 import './notesController.css';
 
-function NoteController() {
+
+function NoteController({notes, setNote}) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleTitleChange = (event) => {
+        const value = event.target.value;
+        setTitle(value);
+    }
+    const handleDescriptionChange = (event) => {
+        const value = event.target.value;
+        setDescription(value);
+    }
+
+    const handleSubmit = () => {
+        const note = {
+            id: Date.now(),
+            title: title,
+            description: description,
+            date: new Date().toLocaleString()
+        };
+        setNote([...notes, note]);
+        setTitle('');
+        setDescription('');
+    }
 
     return (
         <div className="container">
-            <input placeholder="Enter Title"></input>
-            <textarea placeholder="Enter Description"></textarea>
-            <button className='submit-btn'>Submit</button>
+            <input value={title} placeholder="Enter Title" onChange={handleTitleChange}></input>
+            <textarea value={description} placeholder="Enter Description" onChange={handleDescriptionChange}></textarea>
+            <button className='submit-btn' onClick={handleSubmit}>Submit</button>
         </div>
     )
 }
